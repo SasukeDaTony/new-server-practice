5b4c188b7270274cfe64866c91f9cd41b268ac47
SasukeDaTony <tonytaylor@pursuit.org>
2024-01-11 12:15:49 -0500

    added error handling

--- app.js
+++ app.js
@@ -12,23 +12,33 @@ app.get("/name", (req, res) => {
 
 app.get("/add/:num1/:num2", (req, res) => {
   // determine the sum of num1 + num1
-  const num1 = parseInt(req.params.num1);
-  const num2 = parseInt(req.params.num2);
-  const sum = num1 + num2;
-  res.send(`The sum of ${num1} and ${num2} is ${sum}`);
+  try {
+    const num1 = parseInt(req.params.num1);
+    const num2 = parseInt(req.params.num2);
+    const sum = num1 + num2;
+    if (isNaN(num1) || isNaN(num2)) {
+      throw new Error("Invalid input. Please provide valid numbers.");
+    }
+    res.send(`The sum of ${num1} and ${num2} is ${sum}`);
+  } catch (error) {
+    //pass the error to the express handler
+    next(error);
+  }
 });
 
 app.get("/repeat-name/:name/:times", (req, res) => {
   // return repeated name n times
-  const name = req.params.name;
-  const times = parseInt(req.params.times);
-  const repeatedNames = name.repeat(times);
-
-  //   let repeatedNames = "";
-  //   for (let i = 0; i < times; i++) {
-  //     repeatedNames += name;
-  //   }
-  res.send(` ${repeatedNames}`);
+  try {
+    const name = req.params.name;
+    const times = parseInt(req.params.times);
+    const repeatedNames = name.repeat(times);
+    if (isNaN(times) || name.includes("!@#$%^&*()")) {
+      throw new Error("Invalid Input please provide a valid name and number");
+    }
+    res.send(` ${repeatedNames}`);
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.get("/is-divisible-by-3/:num", (req, res) => {
@@ -74,6 +84,11 @@ app.get("/halving-sum-algorithm/:num", (req, res) => {
   res.send(
     `The result of taking the sum of exponential halves for the number ${num} is ${result} using the halvingSum algorithm.`
   );
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong! http:500 error");
 });
 
 module.exports = app;
